bfc68b54c4509917b018da5dead7493c968413ff
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-30 16:03:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-f36yym
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-f36yym)
---
+++ index.js
@@ -0,0 +1,5 @@
+// if input contain number or not
+function myfun(y){
+  console.log(number.isinteger(y));
+}
+myfun(10 / 'vt');
